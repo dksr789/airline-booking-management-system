04c6180c795b22bd818906da1ca0dbeb3fec123e
Deepak Singh <54103399+dksr789@users.noreply.github.com>
2024-03-18 01:49:34 +0530

    Update database.js
    bjhkbjhb

--- models/database.js
+++ models/database.js
@@ -1,20 +1,10 @@
 import { createConnection } from "mysql2";
-import {
-  MYSQL_HOST,
-  MYSQL_USER,
-  MYSQL_PASSWORD,
-  MYSQL_DATABASE,
-} from "../utils/config.js";
-import { SCHEMA } from "./schema.js";
-import { DATA } from "./data.js";
-import { FLIGHT_DATE_DATA } from "./flight_date_data.js";
-import { STORED_OBJECTS } from "./stored_objects.js";
-import { separateSqlCommands } from "./parser.js";
+import { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE } from "../utils/config.js";
 
 class Database {
   constructor() {
     this.db = createConnection({
-      host: "us-cluster-east-01.k8s.cleardb.net",
+      host: "sql6.freesqldatabase.com",
       user: MYSQL_USER,
       password: MYSQL_PASSWORD,
       database: MYSQL_DATABASE,
@@ -22,98 +12,25 @@ class Database {
   }
 
   connect = () => {
-    var that = this;
     return new Promise((resolve, reject) => {
-      that.db
+      this.db
         .promise()
         .connect()
-        .then((res) => {
+        .then(() => {
           resolve();
         })
-        .catch((err) => reject());
-    });
-  };
-
-  executeQuery = (sqlQuery, params) => {
-    var that = this;
-    return new Promise((resolve, reject) => {
-      that.db
-        .promise()
-        .query(sqlQuery, params)
-        .then((res) => {
-          resolve({ data: res[0] });
-        })
-        .catch((error) => {
-          reject({ error: error.message });
-        });
-    });
-  };
-
-  importSchema = () => {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const sqlSchema = separateSqlCommands(SCHEMA);
-        for (let i = 0; i < sqlSchema.length; i++)
-          await db.executeQuery(sqlSchema[i]);
-
-        resolve();
-      } catch (err) {
-        reject();
-      }
-    });
-  };
-
-  importData = () => {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const sqlData = separateSqlCommands(DATA);
-        for (let i = 0; i < sqlData.length; i++)
-          await db.executeQuery(sqlData[i]);
-
-        resolve();
-      } catch (err) {
-        reject();
-      }
-    });
-  };
-
-  importStoredObjects = () => {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const sqlStoredObjects = separateSqlCommands(STORED_OBJECTS);
-        for (let i = 0; i < sqlStoredObjects.length; i++)
-          await db.executeQuery(sqlStoredObjects[i]);
-
-        resolve();
-      } catch (err) {
-        reject();
-      }
-    });
-  };
-
-  importFlightDateData = () => {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const sqlFlightDateData = separateSqlCommands(FLIGHT_DATE_DATA);
-        for (let i = 0; i < sqlFlightDateData.length; i++)
-          await db.executeQuery(sqlFlightDateData[i]);
-
-        resolve();
-      } catch (err) {
-        reject();
-      }
+        .catch((err) => reject(err));
     });
   };
 
   init = async () => {
     console.log("Connecting to DB...");
-    await this.connect();
-    console.log("Connected to DB! Importing Data...");
-    //await this.importSchema();
-    //await this.importData();
-    //await this.importStoredObjects();
-    //await this.importFlightDateData();
-    console.log("Data Imported!");
+    try {
+      await this.connect();
+      console.log("Connected to DB!");
+    } catch (error) {
+      console.error("Failed to connect to DB:", error);
+    }
   };
 }
 
